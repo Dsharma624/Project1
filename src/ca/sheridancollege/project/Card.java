5efb0078a5e70498d1a4fb19c203b4128b997d5c
CODE.BIER <59924431+a-biernacki@users.noreply.github.com>
2020-04-18 20:13:18 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -1,19 +1,15 @@
-/**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
- */
-package ca.sheridancollege.project;
-
-/**
- * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the
- * code should remember to add themselves as a modifier.
+/*
+ * Arianna Biernacki & Devi Sharma
+ * PROG24178 - Software Design
+ * Deliverable 3
+ * Winter Term - 2020
  *
- * @author dancye, 2018
  */
-public class Card {
+package Game;
+
+public abstract class Card {
 
     public String suit; //"H", "D", "S", "C"
     public int rank; //1=Ace, 11=J, 12=Q, 13=K
-}//end class
+}
+
